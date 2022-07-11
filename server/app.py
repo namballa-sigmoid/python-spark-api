7b9d1593d8c5646a8025ebf2b5391774ba939d78
namballa-sigmoid <namballam@sigmoidanalytics.com>
2022-07-11 17:33:09 +0530

    Assignment Updated

--- server/app.py
+++ server/app.py
@@ -1,9 +1,12 @@
-from pyspark.sql import SparkSession
-from flask import Flask
-import json
-from
 import sys
 sys.path.append('../')
+sys.path.insert(0, '../utils')
+
+from pyspark.sql import SparkSession
+from flask import Flask, render_template
+import json
+import pandas
+from utils.queries import Queries
 
 
 spark = SparkSession \
@@ -12,15 +15,99 @@ spark = SparkSession \
     .getOrCreate()
 
 df = spark.read.csv('../csv_data/', header=True, inferSchema=True).drop('_c0')
-
+df.createOrReplaceTempView('stocks')
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    q1_1 = '<a href="http://127.0.0.1:5000/get_max_moved_stocks_per_day/positive">' + 'Query: 1.1 ==>  get_max_moved_stocks_per_day_positive_direction' + '</a><br>'
+    q1_2 = '<a href="http://127.0.0.1:5000/get_max_moved_stocks_per_day/negative">' + 'Query: 1.2 ==>  get_max_moved_stocks_per_day_negative_direction' + '</a><br>'
+    q2 = '<a href="http://127.0.0.1:5000/get_most_traded_stock_per_day">' + 'Query: 2 ==>  get_most_traded_stock_per_day' + '</a><br>'
+    q3 = '<a href="http://127.0.0.1:5000/get_max_gap_per_stock_per_day">' + 'Query: 3 ==>  get_max_gap_per_stock_per_day' + '</a><br>'
+    q4 = '<a href="http://127.0.0.1:5000/get_most_moved_stock">' + 'Query: 4 ==>  get_most_moved_stock' + '</a><br>'
+    q5 = '<a href="http://127.0.0.1:5000/get_stdv_per_stock">' + 'Query: 5 ==>  get_stdv_per_stock' + '</a><br>'
+    q6 = '<a href="http://127.0.0.1:5000/get_mean_median_per_stock">' + 'Query: 6 ==>  get_mean_median_per_stock' + '</a><br>'
+    q7 = '<a href="http://127.0.0.1:5000/get_avg_volume_per_stock">' + 'Query: 7 ==>  get_avg_volume_per_stock' + '</a><br>'
+    q8 = '<a href="http://127.0.0.1:5000/get_stock_with_max_avg_volume">' + 'Query: 8 ==>  get_stock_with_max_avg_volume' + '</a><br>'
+    q9 = '<a href="http://127.0.0.1:5000/get_peak_low_prices_per_stock">' + 'Query: 9 ==>  get_peak_low_prices_per_stock' + '</a><br>'
+    return q1_1 + q1_2 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9
 
-@app.route("/get_max_moved_stocks_per_day")
-def get_max_moved_stocks_per_day():
-    res_df = Queries.get_max_moved_stocks_per_day(spark, df)
+
+@app.route("/get_max_moved_stocks_per_day/<direction>")
+def get_max_moved_stocks_per_day(direction):
+    try:
+        spark.catalog.dropTempView("query")
+    except Exception as e:
+        print('Temp View not created!')
+        
+    res_df = Queries.get_max_moved_stocks_per_day(spark, df, direction)
     res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
 
+
+@app.route("/get_most_traded_stock_per_day")
+def get_most_traded_stock_per_day():
+    res_df = Queries.get_most_traded_stock_per_day(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
+
+
+@app.route("/get_max_gap_per_stock_per_day")
+def get_max_gap_per_stock_per_day():
+    res_df = Queries.get_max_gap_per_stock_per_day(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
+
+
+@app.route("/get_most_moved_stock")
+def get_most_moved_stock():
+    res_df = Queries.get_most_moved_stock(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
+
+
+@app.route("/get_stdv_per_stock")
+def get_stdv_per_stock():
+    res_df = Queries.get_stdv_per_stock(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
+
+
+@app.route("/get_mean_median_per_stock")
+def get_mean_median_per_stock():
+    res_df = Queries.get_mean_median_per_stock(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
+
+
+@app.route("/get_avg_volume_per_stock")
+def get_avg_volume_per_stock():
+    res_df = Queries.get_avg_volume_per_stock(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
+
+
+@app.route("/get_stock_with_max_avg_volume")
+def get_stock_with_max_avg_volume():
+    res_df = Queries.get_stock_with_max_avg_volume(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
+
+
+@app.route("/get_peak_low_prices_per_stock")
+def get_peak_low_prices_per_stock():
+    res_df = Queries.get_peak_low_prices_per_stock(spark, df)
+    res_df.show()
+    pandas_df = res_df.toPandas()
+    return render_template('table.html', tables=[pandas_df.to_html(classes='data')], titles=pandas_df.columns.values)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
---
+++ utils/queries.py
@@ -0,0 +1,59 @@
+
+class Queries:
+    # Q-1
+    @staticmethod
+    def get_max_moved_stocks_per_day(spark, df, direction):
+        df.createOrReplaceTempView('stocks')
+        spark.sql('CREATE TEMPORARY VIEW query AS (SELECT Date, Stock, UpChange, DownChange, RANK() OVER (PARTITION BY Date ORDER BY UpChange DESC) AS MaxUpRank, RANK() OVER (PARTITION BY Date ORDER BY DownChange) AS MaxDownRank FROM (SELECT Stock, Date, ((High - Open)/Open) AS UpChange, ((Low - Open)/Open) AS DownChange FROM stocks))')
+        if direction == 'positive':
+            return spark.sql('SELECT * FROM query WHERE MaxUpRank = 1')
+        return spark.sql('SELECT * FROM query WHERE MaxDownRank = 1')
+
+    # Q-2
+    @staticmethod
+    def get_most_traded_stock_per_day(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT * FROM (SELECT Date, Stock, Volume, RANK() OVER (PARTITION BY Date ORDER BY Volume DESC) AS MostTradedRank FROM stocks) WHERE MostTradedRank=1')
+
+    # Q-3
+    @staticmethod
+    def get_max_gap_per_stock_per_day(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT T3.Stock AS Stock, T3.Date AS Cur_Date, T4.Date AS Prev_Date, T3.Open AS Cur_Open, T4.Close AS Prev_Close, (T3.Open - T4.Close) AS Gap, RANK() OVER (PARTITION BY T3.Date ORDER BY (T3.Open - T4.Close) DESC) AS GapRank FROM (SELECT T1.Date AS Cur_Date, MAX(T2.Date) AS Prev_Date FROM stocks AS T1 INNER JOIN stocks AS T2 WHERE T1.Date > T2.Date GROUP BY Cur_Date ORDER BY Cur_Date) AS subTable INNER JOIN stocks AS T3 ON subTable.Cur_Date = T3.Date INNER JOIN stocks AS T4 ON subTable.Prev_Date = T4.Date WHERE T3.Stock = T4.Stock LIMIT 1')
+
+    # Q-4
+    @staticmethod
+    def get_most_moved_stock(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT T1.Stock, min_date, max_date, (T2.Close - T1.Close) AS StockMoved, ROW_NUMBER() OVER (ORDER BY (T2.Close - T1.Close) DESC) AS MovementRank FROM (SELECT MIN(DATE) AS min_date, MAX(DATE) AS max_date FROM stocks) AS Subtable INNER JOIN stocks AS T1 ON Subtable.min_date = T1.Date INNER JOIN stocks AS T2 ON Subtable.max_date = T2.Date WHERE T1.Stock = T2.Stock LIMIT 1')
+
+    # Q-5
+    @staticmethod
+    def get_stdv_per_stock(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT Stock, STDDEV(Close) AS standard_deviation FROM stocks GROUP BY Stock')
+
+    # Q-6
+    @staticmethod
+    def get_mean_median_per_stock(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT T1.Stock, Mean, Median FROM (SELECT Stock, AVG(Close) AS Median FROM (SELECT Stock, Close, rn, (CASE WHEN cn%2=0 THEN (cn DIV 2) ELSE (cn DIV 2) + 1 END) AS m1, (cn DIV 2) + 1 AS m2 FROM (SELECT Stock, Close, ROW_NUMBER() OVER (PARTITION BY Stock ORDER BY Close) AS rn, COUNT(Close) OVER (PARTITION BY Stock) AS cn FROM stocks)) WHERE rn BETWEEN m1 AND m2 GROUP By Stock) AS T1 INNER JOIN (SELECT Stock, AVG(Close) AS Mean FROM stocks GROUP BY Stock) AS T2 ON T1.Stock = T2.Stock')
+
+    # Q-7
+    @staticmethod
+    def get_avg_volume_per_stock(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT Stock, AVG(Volume) AS Avg_Volume FROM stocks GROUP BY Stock')
+
+    # Q-8
+    @staticmethod
+    def get_stock_with_max_avg_volume(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT Stock, AVG(Volume) AS Avg_Volume FROM stocks GROUP BY Stock ORDER BY Avg_Volume DESC LIMIT 1')
+
+    # Q-9
+    @staticmethod
+    def get_peak_low_prices_per_stock(spark, df):
+        df.createOrReplaceTempView('stocks')
+        return spark.sql('SELECT Stock, MAX(High) AS Highest_Price, MIN(Low) AS Lowest_Price FROM stocks GROUP BY Stock')
+
